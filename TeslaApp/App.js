9a6770734ec57c42deb029044332013bbe6eac19
cesareuseche <cesareuseche@gmail.com>
2020-12-28 18:38:40 -0400

    created the CarItem component

--- TeslaApp/App.js
+++ TeslaApp/App.js
@@ -5,46 +5,19 @@
  * @format
  * @flow strict-local
  */
-
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  ImageBackground,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+import React from 'react'
+import { StatusBar } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import CarItem from './Components/CarItem';
+export default function App() {
   return (
     <>
       <View style={styles.container}>
-        <View style={styles.carContainer}>
-          <ImageBackground
-            source={require('../TeslaApp/img/ModelX.jpeg')}
-            style={styles.image}
-          />
-
-          <View style={styles.titles}>
-            <Text style={styles.title}>Model S</Text>
-            <Text style={styles.subtitle}>Starting at $69,999</Text>
-          </View>
-        </View>
+        <CarItem />
         <StatusBar style="auto" />
       </View>
-
     </>
-  );
+  )
 };
 
 const styles = StyleSheet.create({
@@ -54,33 +27,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-
-  carContainer: {
-    width: '100%',
-    height: '100%',
-  },
-
-  titles: {
-    marginTop: '30%',
-    width: '100%',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 40,
-    fontWeight: '600',
-  },
-  subtitle: {
-    fontSize: 16,
-    color: '#5c5e62',
-  },
-
-  image: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
-    position: 'absolute',
-  }
-
-});
-
-export default App;
+})
---
+++ TeslaApp/Components/CarItem.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { View, Text, ImageBackground } from 'react-native'
+import styles from './CartItemStyles';
+
+
+function CarItem() {
+    return (
+        <View style={styles.carContainer}>
+            <ImageBackground
+                source={require('../img/ModelX.jpeg')}
+                style={styles.image}
+            />
+
+            <View style={styles.titles}>
+                <Text style={styles.title}>Model S</Text>
+                <Text style={styles.subtitle}>Starting at $69,999</Text>
+            </View>
+        </View>
+    )
+}
+
+export default CarItem;
